e0d7d14fade98913a792abfd23596a7fe994921b
chickencuber <n1batman728@gmail.com>
2025-05-09 18:12:26 -0500

    fixed ls

--- cmd/ls.js
+++ cmd/ls.js
@@ -2,6 +2,17 @@ const path = args[0]?.toPath() || Shell.localVars.workingDir;
 if (!await FS.exists(path)) {
   return "path doesn't exist";
 }
+async function print(path) {
+    const type = await getMetaFromPath(path).type;
+    const name = FS.normalizePath(path).at(-1);
+    if(type === "dir" ) {
+        return "dir: " + k + "/"
+    } else if (type === "symlink") {
+        return "symlink: " + k;
+    } else {
+        return "file: " + k;
+    }
+}
 const contents = await FS.getMetaFromPath(path);
 if (contents.type === "dir") {
   return (await Promise.all(contents.contents
