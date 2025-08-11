d63397db4a51976ee03a29c6d981ffe923322832
chickencuber <n1batman728@gmail.com>
2025-08-11 18:05:15 -0500

    did thing

--- bin/std/sh/lib.js
+++ bin/std/sh/lib.js
@@ -1,21 +1,29 @@
 return async (code, dir = "", args = []) => {
-  code = code.split("\n").map((v) => v.trim());
-  const vars = {
-    dir,
-    ...args,
-    args: args.join(" "),
-  };
-  for (const l of code) {
-    if (!l || l.startsWith("#")) continue;
-    const r = await Shell.run(
-      l
-        .replaceAll(/(?<!\\)\$([A-Za-z0-9]+)/gm, (_, name) => {
-          return vars[name];
-        })
-        .replaceAll(/\\\$([A-Za-z0-9]+)/gm, (_, name) => {
-          return "$" + name;
-        })
-    );
-    if (r) Shell.terminal.add(r + "\n");
-  }
+    code = code.split("\n").map((v) => v.trim());
+    const _vars = {
+        dir,
+        ...args,
+        args: args.join(" "),
+    };
+    const vars = new Proxy(_vars, {
+        get(target, p) {
+            if(p.startsWith("$")) {
+                return Shell.localVars[p.slice(1)];
+            }
+            return target[p];
+        }
+    })
+    for (const l of code) {
+        if (!l || l.startsWith("#")) continue;
+        const r = await Shell.run(
+            l
+            .replaceAll(/(?<!\\)\$([A-Za-z0-9]+)/gm, (_, name) => {
+                return vars[name];
+            })
+            .replaceAll(/\\\$([A-Za-z0-9]+)/gm, (_, name) => {
+                return "$" + name;
+            })
+        );
+        if (r) Shell.terminal.add(r + "\n");
+    }
 };
